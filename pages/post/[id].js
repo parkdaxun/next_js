db6d855e6733f42ffdb344bd65f9a08830327241
parkdaxun <parkde0207@gmail.com>
2022-07-13 16:05:29 +0900

    [fix]

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,17 +1,17 @@
-import { useFouter } from "next/router";
-import { useEffect } from "react";
+import {useRouter} from "next/router";
+import {useEffect} from "react";
 
-export default function Home() {
-    const router = useFouter();
+export default function PostDetail() {
+    const router = useRouter();
     const { id = 0 } = router.query;
 
     useEffect(() => {
-        if(!/^[0-9]+$/test(id) || id === undefined) {
+        if (!/^[0-9]+$/.test(id) || id === undefined) {
             router.replace("/");
-        } else {
-            console.info("post id" + id);
         }
-    }, []);
+    }, [id]);
 
-    return <div>ID : {id}</div>;
+    return (
+        <div>ID: {id}</div>
+    )
 }
